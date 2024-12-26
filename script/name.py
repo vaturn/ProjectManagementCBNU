315b3b0ce4e71b43bba1ddc689747e71ba53205e
vaturn <will6867@naver.com>
2024-12-26 14:49:33 +0900

    Init: Create Script File

---
+++ script/name.py
@@ -0,0 +1,5 @@
+# 명명 규칙에 따른 알고리즘을 채울 예정
+
+def naming():
+    return "something"
+
---
+++ script/upload.py
@@ -0,0 +1,12 @@
+# 터미널로 실행할 예정
+
+# ex ) upload "파일이름" , upload . , upload "파일이름" -r
+
+def upload():
+    return
+
+def upload_raw():
+    return
+
+def command_parsing():
+    return
